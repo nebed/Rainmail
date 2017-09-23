4158eba8bc7ac9d113af499486ec4341384fe24f
Uchenna Nebedum <nebeduch@gmail.com>
2017-09-23 08:23:50 +0100

    Update prompt_for_options.py
    Python Function to get user's choice for express settings or user_defined settings

--- prompt_for_options.py
+++ prompt_for_options.py
@@ -1,4 +1,5 @@
 def prompt_for_options():
+	
 	while True:
 		try:
 			choose_way = int(raw_input("Enter 1 for Express Settings or Enter 2 to Create Your Own Setup : "))
@@ -10,9 +11,11 @@ def prompt_for_options():
 			continue
 		else:
 			break
+			
 	if choose_way == 1:
 		print("You Have Chosen Express Settings")
 	elif choose_way ==2:
 		print("You Will Need to Specify Your own Settings")
+		
 	return choose_way
        
