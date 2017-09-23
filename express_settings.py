841e9c640eb4ecb958ea516147725d9669277853
Uchenna Nebedum <nebeduch@gmail.com>
2017-09-23 01:36:27 +0100

    Update express_settings.py

--- express_settings.py
+++ express_settings.py
@@ -285,31 +285,4 @@ def express_settings():
 
 
 
-'''
 
-# See in the config.cfg file for possible values
-config = {
-    'use_own_ip': True,
-    'keywords': keywords,
-    'search_engines': ['google'],   #add other search engines if you need
-    'num_pages_for_keyword': 50,
-    'scrape_method': 'selenium',
-    'sel_browser': 'chrome',
-}
-
-try:
-	search = scrape_with_config(config)
-except GoogleSearchError as e:
-	print(e)'''
-
-'''f = open('searchresultlinks.txt','w')    #indicate name of output file with google search links
-sys.stdout = f
-path= '/home/Desktop'
-
-for serp in search.serps:
-	#print(serp)
-
-	for link in serp.links:
-		#print(link)
-		print (link, f)  # or f.write('...\n')  print each link to the file
-f.close()'''
